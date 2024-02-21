ea20da8bc8bfd6fe7597fd36112c7ea349f53562
Hien Tang <quochien207@gmail.com>
2024-02-21 14:52:17 +0700

    create project theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import "./index.css";
+import { PALETTE_COLOR } from "./utils/constant.js";
+
+let theme = createTheme({
+  typography: {
+    fontFamily: '"Work Sans", sans-serif',
+  },
+
+  palette: {
+    primary: {
+      main: PALETTE_COLOR.red,
+      dark: PALETTE_COLOR.darkRed,
+      light: PALETTE_COLOR.lightRed,
+    },
+    secondary: {
+      main: PALETTE_COLOR.brown,
+      dark: PALETTE_COLOR.darkBrown,
+      light: PALETTE_COLOR.lightBrown,
+    },
+    tertiary: {
+      main: PALETTE_COLOR.yellow,
+      dark: PALETTE_COLOR.darkYellow,
+      light: PALETTE_COLOR.lightYellow,
+    },
+    gray: {
+      main: PALETTE_COLOR.gray,
+      dark: PALETTE_COLOR.darkGray,
+      light: PALETTE_COLOR.lightGray,
+    },
+    white: {
+      main: PALETTE_COLOR.white,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/utils/constant.js
@@ -0,0 +1,19 @@
+export const PALETTE_COLOR = {
+  red: "#ab0101",
+  darkRed: "#780101",
+  lightRed: "#c44d4d",
+
+  brown: "#572f2f",
+  darkBrown: "#462626",
+  lightBrown: "#795959",
+
+  yellow: "#f8e60d",
+  darkYellow: "#aea109",
+  lightYellow: "#faee56",
+
+  gray: "#9ca3af",
+  darkGray: "#4b5563",
+  lightGray: "#e5e7eb",
+
+  white: "#fff",
+};
