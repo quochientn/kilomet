16ce198010144768cb06a9dc3ddf76b824428b85
Hien Tang <quochien207@gmail.com>
2024-02-29 17:06:39 +0700

    update training carousel display on mobile and tablet

--- src/ui/TrainingCarousel.jsx
+++ src/ui/TrainingCarousel.jsx
@@ -1,33 +1,47 @@
-import { styled, useTheme } from "@mui/material";
+import { Box, styled, useTheme } from "@mui/material";
 
 const StyledImg = styled("img")`
   height: 100%;
+
+  @media screen and (max-width: 900px) {
+    width: 100%;
+  }
 `;
 
 function TrainingCarousel({ images }) {
   const theme = useTheme();
   return (
-    <swiper-container
-      free-mode={JSON.stringify({
-        enabled: true,
-        sticky: true,
-      })}
-      slides-per-view="auto"
-      space-between="5"
-      loop="true"
-      autoplay-delay="5000"
-      pagination-dynamic-bullets="true"
-      style={{
-        height: "30rem",
-        "--swiper-pagination-color": `${theme.palette.primary.main}`,
-      }}
-    >
-      {images.map((img) => (
-        <swiper-slide key={img} style={{ height: "100%", width: "auto" }}>
-          <StyledImg src={img} alt="Training" />
-        </swiper-slide>
-      ))}
-    </swiper-container>
+    <>
+      <Box sx={{ display: { md: "block", xs: "none" } }}>
+        <swiper-container
+          free-mode={JSON.stringify({
+            enabled: true,
+            sticky: true,
+          })}
+          slides-per-view="auto"
+          space-between="5"
+          loop="true"
+          autoplay-delay="5000"
+          pagination-dynamic-bullets="true"
+          style={{
+            height: "30rem",
+            "--swiper-pagination-color": `${theme.palette.primary.main}`,
+          }}
+        >
+          {images.map((img) => (
+            <swiper-slide key={img} style={{ height: "100%", width: "auto" }}>
+              <StyledImg src={img} alt="Training" />
+            </swiper-slide>
+          ))}
+        </swiper-container>
+      </Box>
+
+      <Box sx={{ display: { md: "none" } }}>
+        {images.map((img) => (
+          <StyledImg src={img} alt="Training" key={img} />
+        ))}
+      </Box>
+    </>
   );
 }
 
